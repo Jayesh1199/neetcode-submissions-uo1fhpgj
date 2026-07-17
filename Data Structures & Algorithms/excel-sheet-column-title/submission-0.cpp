07824b6d0196627c22367b1d94bd8451e8219de3
Jayesh Mohan <55408029+Jayesh1199@users.noreply.github.com>
2026-07-17 19:19:39 -0400

    Add: excel-sheet-column-title - submission-0

---
+++ Data Structures & Algorithms/excel-sheet-column-title/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        
+        string result = "";
+
+        while(columnNumber > 0){
+            columnNumber--;
+            int remainder = columnNumber % 26;
+
+            char currentLetter = 'A' + remainder;
+
+            result = result + currentLetter;
+
+            columnNumber /= 26;
+
+        }
+        reverse(result.begin(), result.end());
+        
+        return result;
+    }
+};
